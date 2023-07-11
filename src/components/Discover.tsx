d86512c9242547d6aeb2d835b59f7260943b54b6
andreubltzr <andreubltzr11@gmail.com>
2023-07-11 17:46:32 +0200

    added routes

---
+++ src/components/Discover.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material";
+
+export const Discover = () => {
+  return (
+    <>
+      <Container
+        sx={{
+          bgcolor: "#2F303A",
+          height: "90vh",
+        }}
+      >
+        Discover
+      </Container>
+    </>
+  );
+};
---
+++ src/components/Join.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material";
+
+export const Join = () => {
+  return (
+    <>
+      <Container
+        sx={{
+          bgcolor: "#2F303A",
+          height: "90vh",
+        }}
+      >
+        Join
+      </Container>
+    </>
+  );
+};
